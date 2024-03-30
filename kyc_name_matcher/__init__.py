9db581101a9df28b4ea44faa836099f2d88a5546
mayur <mayur@contrailtech.com>
2024-03-30 14:03:00 +0530

    Initial Project upload

---
+++ kyc_name_matcher/__init__.py
@@ -0,0 +1,1 @@
+from .main import match_aadhar
---
+++ tests.py
@@ -0,0 +1,5 @@
+from kyc_name_matcher.main import match_aadhar
+
+aadhar_name = "Deepak R Fegde"
+pan_name = "Mayur Rajendra Fegde"
+print(match_aadhar(aadhar_name=aadhar_name, pan_name=pan_name))
